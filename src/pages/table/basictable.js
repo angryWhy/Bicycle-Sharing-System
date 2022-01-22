1359aa4568ae4fcb810e6261f0495ac4fa27de4b
angryWhy <875978822@qq.com>
2022-01-22 22:02:20 +0800

    1.22

--- src/pages/table/basictable.js
+++ src/pages/table/basictable.js
@@ -1,5 +1,7 @@
-import React, { memo } from 'react';
-import { Card,Table } from 'antd';
+import React, { memo,useEffect,useState } from 'react';
+import { Card,Table,Spin } from 'antd';
+import {LoadingOutlined} from "@ant-design/icons"
+import axios from "axios"
 export default memo(function Basictable() {
     const dataSource=[
         {
@@ -23,7 +25,7 @@ export default memo(function Basictable() {
             time:"8:00"
         },
         {
-            id:"0",
+            id:"2",
             userName:"jack",
             sex:"1",
             state:"1",
@@ -33,7 +35,7 @@ export default memo(function Basictable() {
             time:"8:00"
         },
         {
-            id:"0",
+            id:"3",
             userName:"poss",
             sex:"1",
             state:"1",
@@ -50,15 +52,24 @@ export default memo(function Basictable() {
         },
         {
             title:"用户名",
-            dataIndex:"userName"
+            dataIndex:"userName",
         },
         {
             title:"性别",
-            dataIndex:"sex"
+            dataIndex:"sex",
+            render(sex){
+                return sex===1?"男":"女"
+            }
         },
         {
             title:"state",
-            dataIndex:"state"
+            dataIndex:"state",
+            render(state){
+                let config={
+                    "1":"风华浪子"
+                }
+                return config[state]
+            }
         },
         {
             title:"兴趣",
@@ -77,9 +88,49 @@ export default memo(function Basictable() {
             dataIndex:"time"
         }
     ]
+    const [selectedRowKeys,setSelect] = useState()
+    const [selectItem,setSelectItem] = useState()
+    useEffect(()=>{
+        axios.get("/api/tableList.json").then(res=>{
+            console.log(res);
+        })
+    },[])
+    dataSource.map((item,index)=>{
+        item.key=index
+    })
+    const [loading,SetLoading] = useState(true)
+    const antIcon = <LoadingOutlined style={{ fontSize:40 }} spin />;
+    const onRowClick=(record,index)=>{
+        let selectKey=[index]
+        setSelectItem(record)
+        setSelect(selectKey)
+
+    }
+    const rowSelection={
+        selectedRowKeys
+    }
   return <div>
+     
       <Card title="基础表格" >
           <Table columns={columns} dataSource={dataSource}></Table>
+      
+      </Card>
+      <Card title="动态表格-loading" >
+      <Spin spinning={loading} indicator={antIcon}  size="large">
+          <Table columns={columns} dataSource={dataSource}></Table>
+        </Spin>
+      </Card>
+      <Card title="mock-单选"  >
+          <Table columns={columns} 
+                 dataSource={dataSource} 
+                 rowSelection={rowSelection}
+                 onRow={(record,index) => {
+                    return {
+                      onClick: event => {onRowClick(record,index)}, // 点击行
+                    };
+                  }}
+                 />
+      
       </Card>
   </div>;
 });
