7d92f4ecef5fa878a046e44c3861a5f341e59a0f
angryWhy <875978822@qq.com>
2022-01-23 17:18:15 +0800

    1.23

--- src/pages/table/basictable.js
+++ src/pages/table/basictable.js
@@ -1,7 +1,8 @@
 import React, { memo,useEffect,useState } from 'react';
-import { Card,Table,Spin } from 'antd';
+import { Card,Table,Spin, Modal, message ,Button} from 'antd';
 import {LoadingOutlined} from "@ant-design/icons"
 import axios from "axios"
+import { pagination } from '../../utils/format-Date';
 export default memo(function Basictable() {
     const dataSource=[
         {
@@ -90,14 +91,24 @@ export default memo(function Basictable() {
     ]
     const [selectedRowKeys,setSelect] = useState()
     const [selectItem,setSelectItem] = useState()
+    const [paga, setPaga] = useState();
+    const [pagedata, setPagedata] = useState();
+    const [curpage, setCurpage] = useState();
     useEffect(()=>{
         axios.get("/api/tableList.json").then(res=>{
-            console.log(res);
-        })
-    },[])
-    dataSource.map((item,index)=>{
+            const a=res.data.result.list.map((item,index)=>{item.key=index;return item})
+            setPaga(res)
+            setPagedata(a)
+        } 
+        
+        )
+        console.log(curpage);
+    },[curpage])
+    const newdataSource=dataSource.map((item,index)=>{
         item.key=index
+        return item
     })
+    console.log(newdataSource);
     const [loading,SetLoading] = useState(true)
     const antIcon = <LoadingOutlined style={{ fontSize:40 }} spin />;
     const onRowClick=(record,index)=>{
@@ -106,31 +117,75 @@ export default memo(function Basictable() {
         setSelect(selectKey)
 
     }
+   
+    // const rowSelection={
+    //     type:"radio",
+    //     selectedRowKeys
+    // }
+    //单选框
     const rowSelection={
+        type:"radio",
         selectedRowKeys
+    }
+    //复选框，选一堆
+    const rowCheckSelection={
+        type:"checkbox",
+        selectedRowKeys,
+        onChange:(selectedRowKeys,selectedRows)=>{
+            setSelect(selectedRowKeys)
+            setSelectItem(selectedRows)
+        }
+    }
+    //多选框删除
+    const handleDeleted = ()=>{
+        let rows=selectItem
+        let id=[]
+        rows.map((item)=>{
+            id.push(item.id)
+            return item
+        })
+        Modal.confirm({
+            title:"删除提示",
+            content:`您确定要删除这些数据吗？${id.join(",")}`,
+            onOk:()=>{
+                //请求数据
+                message.success("删除成功")
+                //把select设置为null和【】
+                setSelect([])
+                setSelectItem(null)
+            }
+        })
     }
   return <div>
      
       <Card title="基础表格" >
-          <Table columns={columns} dataSource={dataSource}></Table>
+          <Table columns={columns} dataSource={newdataSource}></Table>
       
       </Card>
-      <Card title="动态表格-loading" >
       <Spin spinning={loading} indicator={antIcon}  size="large">
-          <Table columns={columns} dataSource={dataSource}></Table>
-        </Spin>
+      <Card title="动态表格-loading" >
+      
+          <Table columns={columns} dataSource={newdataSource}></Table>
+       
       </Card>
+      </Spin>
       <Card title="mock-单选"  >
           <Table columns={columns} 
-                 dataSource={dataSource} 
-                 rowSelection={rowSelection}
+                 dataSource={newdataSource} 
+                 rowSelection={rowCheckSelection}
                  onRow={(record,index) => {
                     return {
                       onClick: event => {onRowClick(record,index)}, // 点击行
                     };
                   }}
                  />
-      
+                 <Button onClick={e=>{handleDeleted()}}>删除</Button>
+      </Card>
+      <Card title="mock-单选"  >
+          <Table columns={columns} 
+                 dataSource={pagedata} 
+                 pagination={e=>{pagination(paga,(page)=>{setCurpage(page)})}}
+                 />
       </Card>
   </div>;
 });
--- src/utils/format-Date.js
+++ src/utils/format-Date.js
@@ -1,3 +1,18 @@
 export const formateDate=(newDate)=>{
     return `${newDate.getFullYear()}-${newDate.getMonth()+1}-${newDate.getHours()}-${newDate.getMinutes()}:${newDate.getSeconds()}`
 }
+export const pagination = (data,callback) =>{
+    return{
+        onChange:(page)=>{
+            callback(page)
+        },
+        current:data.data.result.page,
+        pageSize:data.data.result.page_size,
+        total:data.data.result.total,
+        showTotal:()=>{
+            return`总条数${data.data.result.total}`
+        },
+        showQuickJumper:true
+    }
+   
+}
